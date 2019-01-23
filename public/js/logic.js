f5bb89acaa3fed331cfb48466df9d3b20216c175
AymanAlQoqa <aalqouqa@gmail.com>
2019-01-23 09:22:40 +0200

    modified logic file relates #3

---
+++ public/js/logic.js
@@ -0,0 +1,46 @@
+
+
+let fetchData = function (searchFor, cb) {
+
+    url = `https://api.openweathermap.org/data/2.5/weather?q=${searchFor}&units=metric&appid=` + apiKeyWeather;
+    //return console.log(url);
+
+
+    function accessData(url, callback) {
+        let xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                response = JSON.parse(xhr.responseText);
+                //console.log(response);
+
+                callback(response);
+            }
+
+        }
+        xhr.open("GET", url);
+        xhr.send();
+    }
+
+    //
+    accessData(url, (response) => {
+        result.name = (response.name);
+        result.main = (response.main);
+        result.description = (response.weather[0].description);
+        //invoke accessData second time
+        let newSearch = response.weather[0].description + ' weather';
+        let newUrl = `http://api.giphy.com/v1/gifs/search?q=${newSearch}&api_key=` + apiKeyGiphy;
+        //console.log(newUrl)
+        accessData(newUrl, (response) => {
+            let index = Math.floor(Math.random() * response.data.length);
+            if (index >= 0 && index < response.data.length)
+                result.imageUrl = (response.data[index].images.fixed_width.url);
+            // console.log(result);
+
+            cb(result);
+        })
+
+    });
+
+
+
+}
