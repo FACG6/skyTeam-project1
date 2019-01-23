4f74614a73aede4c85bedcdaf7d7294640f2fff2
AymanAlQoqa <aalqouqa@gmail.com>
2019-01-23 13:48:40 +0200

    edit logic, dom, and html file relates #4 #3

--- public/js/dom.js
+++ public/js/dom.js
@@ -1,20 +1,28 @@
 let searchText = document.getElementById('searchFor');
 let searchBtn = document.getElementById('searchBtn');
-const result = {};
+
 searchBtn.addEventListener('click', (e) => {
     e.preventDefault();
     fetchData(searchText.value, (res) => {
         console.log(res);
-        //load weather
-        document.querySelector('#cityName').textContent = 'City name: ' + res.name;
-        document.querySelector('#temp').textContent = 'Temperature: ' + res.main.temp + ' celsius';
-        document.querySelector('#humidity').textContent = 'Humidity: ' + res.main.humidity;
-        document.querySelector('#pressure').textContent = 'Pressure: ' + res.main.pressure;
-        document.querySelector('#state').textContent = 'State: ' + res.description;
+        if (res.cod == 404) {
+            getClearAll(res);
+            alert(res.message);
+            return -1;
 
-        // load image
-        document.querySelector('.image').src = res.imageUrl;
+        } else {
 
+
+            //load weather
+            document.querySelector('#cityName').textContent = 'City name: ' + getCityName(res);
+            document.querySelector('#temp').textContent = 'Temperature: ' + getTemp(res);
+            document.querySelector('#humidity').textContent = 'Humidity: ' + getHumidity(res);
+            document.querySelector('#pressure').textContent = 'Pressure: ' + getPressure(res);
+            document.querySelector('#state').textContent = 'State: ' + getDescription(res);
+
+            // load image
+            document.querySelector('.image').src = getImageUrl(res);
+        }
     });
 
 
