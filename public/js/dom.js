709136f425400bab62f4ae470ca7f2de035b8058
Nareman <naremanmohhilles@gmail.com>
2019-01-23 15:25:58 +0200

    edit logic and dom

--- public/js/dom.js
+++ public/js/dom.js
@@ -1,29 +1,35 @@
 let searchText = document.getElementById('searchFor');
 let searchBtn = document.getElementById('searchBtn');
 
+function updateStatus(res){
+    document.querySelector('#cityName').textContent = 'City name: ' + getCityName(res);
+    document.querySelector('#temp').textContent = 'Temperature: ' + getTemp(res);
+    document.querySelector('#humidity').textContent = 'Humidity: ' + getHumidity(res);
+    document.querySelector('#pressure').textContent = 'Pressure: ' + getPressure(res);
+    document.querySelector('#state').textContent = 'State: ' + getDescription(res);
+    // load image
+    document.querySelector('.image').src = getImageUrl(res);
+}
 searchBtn.addEventListener('click', (e) => {
+    if(searchText.value !== ''){
+       
+        
+    
     e.preventDefault();
     fetchData(searchText.value, (res) => {
         console.log(res);
         if (res.cod == 404) {
             getClearAll(res);
             alert(res.message);
-            return -1;
-
+            updateStatus(res); 
         } else {
-
-
+          
+       updateStatus(res); 
             //load weather
-            document.querySelector('#cityName').textContent = 'City name: ' + getCityName(res);
-            document.querySelector('#temp').textContent = 'Temperature: ' + getTemp(res);
-            document.querySelector('#humidity').textContent = 'Humidity: ' + getHumidity(res);
-            document.querySelector('#pressure').textContent = 'Pressure: ' + getPressure(res);
-            document.querySelector('#state').textContent = 'State: ' + getDescription(res);
-
-            // load image
-            document.querySelector('.image').src = getImageUrl(res);
+         
         }
     });
 
-
+    }
+    else alert('please enter valid city name')
 })
